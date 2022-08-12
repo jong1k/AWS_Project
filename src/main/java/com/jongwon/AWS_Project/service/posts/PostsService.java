78448ef2f5560274ab4613526d1eeb659d703041
jongwon_kim <jongwon_kim@tmax.co.kr>
2022-08-12 11:40:27 +0900

    Posts 도메인 생성, 등록/조회/수정 API 생성 및 테스트 생성

---
+++ src/main/java/com/jongwon/AWS_Project/service/posts/PostsService.java
@@ -0,0 +1,34 @@
+package com.jongwon.AWS_Project.service.posts;
+
+import com.jongwon.AWS_Project.domain.posts.Posts;
+import com.jongwon.AWS_Project.domain.posts.PostsRepository;
+import com.jongwon.AWS_Project.web.dto.PostsResponseDto;
+import com.jongwon.AWS_Project.web.dto.PostsSaveRequestDto;
+import com.jongwon.AWS_Project.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+        return id;
+    }
+
+    public PostsResponseDto findById (Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        return new PostsResponseDto(entity);
+    }
+}
---
+++ src/main/java/com/jongwon/AWS_Project/web/dto/PostsUpdateRequestDto.java
@@ -0,0 +1,18 @@
+package com.jongwon.AWS_Project.web.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class PostsUpdateRequestDto {
+    private String title;
+    private String content;
+
+    @Builder
+    public PostsUpdateRequestDto(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
