653ca2cbb24394111d06c41ae27bb16c8b31b9c2
jongwon_kim <jongwon_kim@tmax.co.kr>
2022-08-17 20:53:33 +0900

    게시글 수정 화면 구성 및 js 업데이트, controller 연결

--- src/main/java/com/jongwon/AWS_Project/web/IndexController.java
+++ src/main/java/com/jongwon/AWS_Project/web/IndexController.java
@@ -1,10 +1,12 @@
 package com.jongwon.AWS_Project.web;
 
 import com.jongwon.AWS_Project.service.posts.PostsService;
+import com.jongwon.AWS_Project.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -21,5 +23,13 @@ public class IndexController {
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
     }
 }
